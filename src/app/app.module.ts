7c4fbba89e542d34bb92c9ee16c4fa0dc97cf9d2
alhanson7210 <alhanson@dons.usfca.edu>
2019-07-30 09:07:34 -0700

    Initial setup in github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolbarHeaderComponent } from './components/toolbar-header/toolbar-header.component';
+import { ToolbarFooterComponent } from './components/toolbar-footer/toolbar-footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarHeaderComponent,
+    ToolbarFooterComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
